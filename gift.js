e3f98ff43e170b764226855a584493d7e74492a2
SuongRanet <mctv9876@gmai.com>
2024-12-25 21:53:53 +0700

    first commit

---
+++ gift.js
@@ -0,0 +1,4 @@
+document.querySelector('.gift-button').addEventListener('click', () => {
+    document.querySelector('.message-box').style.display = 'block';
+    document.querySelector('.gift-button').style.display = 'none';
+});
