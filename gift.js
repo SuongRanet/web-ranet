83790a02ae56394244125cf486a7756eb33d7624
SuongRanet <154446025+SuongRanet@users.noreply.github.com>
2024-12-25 22:19:27 +0700

    Update gift.js

--- gift.js
+++ gift.js
@@ -1,4 +1,6 @@
+let name1 = document.querySelector('.merry');
 document.querySelector('.gift-button').addEventListener('click', () => {
     document.querySelector('.message-box').style.display = 'block';
     document.querySelector('.gift-button').style.display = 'none';
+    name1.innerHTML = 'Merry Christmas! Oun ying🎄✨';
 });
